65c1cf339cff9dac571564c706254db105d4f84b
andreaspro9 <andkarido@gmail.com>
2021-05-26 17:59:44 +0300

    style(Home): Added animation on title and shadow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
 	<header class="home">
-		<h1 class="home-title">Front-End Starter Kit</h1>
+		<h1 class="home-title">
+			Front-End Starter Kit
+		</h1>
+		<div class="home-title-shadow"></div>
 		<router-link class="home-btn" to="/about">Get Started</router-link>
 	</header>
 </template>
@@ -26,12 +29,24 @@ export default {
 	font-weight: bold;
 	font-size: 4rem;
 	color: var(--secondary-color);
+	animation: hover-state 2s ease-in-out infinite alternate forwards;
 	text-shadow: 1px 1px var(--secondary-color-light),
 		2px 2px var(--secondary-color-light),
 		3px 3px var(--secondary-color-light),
 		4px 4px var(--secondary-color-light),
 		5px 5px var(--secondary-color-light),
 		6px 6px var(--secondary-color-light);
+}
+
+.home-title-shadow {
+	position: absolute;
+	top: 480px;
+	width: 10rem;
+	height: 10px;
+	opacity: 0.8;
+	border-radius: 50%;
+	animation: shrink-shadow 4s 0.7s ease-in-out infinite;
+	background: rgba(0, 0, 0, 0.25);
 }
 
 .home-btn {
@@ -48,5 +63,21 @@ export default {
 .home-btn:hover {
 	font-weight: bold;
 	background: #ac4590;
+}
+
+@keyframes hover-state {
+	0% {
+		transform: translateY(0px);
+	}
+
+	100% {
+		transform: translateY(-25px);
+	}
+}
+
+@keyframes shrink-shadow {
+	50% {
+		opacity: 0;
+	}
 }
 </style>
