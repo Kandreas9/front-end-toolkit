6c421014bb3363638fba7a8e6db2a1131b60280c
andreaspro9 <andkarido@gmail.com>
2021-05-26 05:10:54 +0300

    styled(Home): Created and styled the home page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<router-view />
 </template>
 
 <style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+:root {
+	--primary-color: #893272;
+	--secondary-color: #f47d5e;
+	--secondary-color-light: #ffb957;
+	--bg-color: #372667;
+}
+
+body {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background-color: var(--bg-color);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+	min-height: 100vh;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<header class="home">
+		<h1 class="home-title">Front-End Starter Kit</h1>
+		<router-link class="home-btn" to="/about">Get Started</router-link>
+	</header>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+};
 </script>
+
+<style>
+.home {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	font-size: 1.3rem;
+}
+
+.home-title {
+	max-width: 8ch;
+	margin-bottom: 1.5rem;
+	text-align: center;
+	font-weight: bold;
+	font-size: 4rem;
+	color: var(--secondary-color);
+	text-shadow: 1px 1px var(--secondary-color-light),
+		2px 2px var(--secondary-color-light),
+		3px 3px var(--secondary-color-light),
+		4px 4px var(--secondary-color-light),
+		5px 5px var(--secondary-color-light),
+		6px 6px var(--secondary-color-light);
+}
+
+.home-btn {
+	width: 10rem;
+	text-align: center;
+	color: var(--secondary-color);
+	text-decoration: none;
+	padding: 0.5rem 0.9rem;
+	background: var(--primary-color);
+	box-shadow: 6px 6px 4px rgba(0, 0, 0, 0.25);
+	border-radius: 27px;
+}
+
+.home-btn:hover {
+	font-weight: bold;
+	background: #ac4590;
+}
+</style>
